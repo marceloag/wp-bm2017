df5a3c56ce0fbbcd0385d109002f243c2cde0393
Marcelo Aguila <web@croop.cl>
2015-12-12 16:00:18 -0300

    bg

--- index.php
+++ index.php
@@ -29,26 +29,6 @@
             </li>
             <?php endwhile; ?>
             <!-- end of the loop -->
-            <li class="lanas sli">
-              <div class="large-5 columns desc">
-                    <h1>Lanas</h1>
-                    <span class="categoria">Articulos de Lana</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur sapiente, dolor! Assumenda veritatis, voluptas, amet voluptatum unde odio, vitae veniam dolores suscipit animi tempora rerum nostrum nam distinctio beatae numquam.</p>
-              </div>
-              <div class="large-7 columns">
-                  <img src="<?php bloginfo('template_directory'); ?>/images/slide01.jpg">
-              </div>
-            </li>
-            <li class="lanas sli">
-              <div class="large-5 columns desc">
-                    <h1>Lanas</h1>
-                    <span class="categoria">Articulos de Lana</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur sapiente, dolor! Assumenda veritatis, voluptas, amet voluptatum unde odio, vitae veniam dolores suscipit animi tempora rerum nostrum nam distinctio beatae numquam.</p>
-              </div>
-              <div class="large-7 columns">
-                  <img src="http://fakeimg.pl/715x354/00CED1/FFF/?text=img+placeholder">
-              </div>
-            </li>
           </ul>
       </div>
   </section>
