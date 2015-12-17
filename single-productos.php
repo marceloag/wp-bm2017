74e15cf8403f70a9d97db36963bd3673aa2be28b
Marcelo Aguila <web@croop.cl>
2015-12-17 10:39:21 -0300

    Single

--- single-productos.php
+++ single-productos.php
@@ -4,7 +4,29 @@
 	<div class="row">
 		<div class="large-10 columns large-centered">
 			<div class="large-5 columns">
-				<?php the_post_thumbnail("producto"); ?>
+				<!-- <?php the_post_thumbnail("producto"); ?> -->
+			<ul class="example-orbit" data-orbit>
+				<?php
+	              	$attachments = get_children(array('post_parent' => $post->ID,
+	                                      'post_status' => 'inherit',
+	                                      'post_type' => 'attachment',
+	                                      'post_mime_type' => 'image',
+	                                      'order' => 'ASC',
+	                                      'orderby' => 'menu_order ID'));
+
+	              	foreach($attachments as $att_id => $attachment) {
+	                  $full_img_url = wp_get_attachment_image($attachment->ID, "producto");
+	                  $metaatt= wp_get_attachment( $attachment->ID );
+	                  ?>
+	                  <li>
+	                      <?php echo $full_img_url;?>
+	                  </li>
+	                 <?php
+	              	}
+        		?>
+
+			</ul>
+
 			</div>
 			<div class="large-7 columns">
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
