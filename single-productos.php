a91b03227cade8abd8db36984ba31a495fc790bf
Marcelo Aguila <web@croop.cl>
2015-12-17 09:47:35 -0300

    Single

--- single-productos.php
+++ single-productos.php
@@ -1,18 +1,23 @@
 <?php get_header(); ?>
 
 <div class="row">
-	<div class="large-5 columns">
-		<?php the_post_thumbnail("producto"); ?>
+	<div class="row">
+		<div class="large-10 columns large-centered">
+			<div class="large-5 columns">
+				<?php the_post_thumbnail("producto"); ?>
+			</div>
+			<div class="large-7 columns">
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<h3><?php the_title(); ?></h3>
+				<?php the_content(); ?>
+				<a href="" class="button tiny alert comprar">
+					<i  class="fa fa-cart-plus"></i>  Comprar
+				</a>
+			<?php endwhile; endif; ?>
+			</div>
+		</div>	
 	</div>
-	<div class="large-7 columns">
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<h3><?php the_title(); ?></h3>
-		<?php the_content(); ?>
-		<a href="" class="button tiny alert comprar">
-			<i  class="fa fa-cart-plus"></i>  Comprar
-		</a>
-	<?php endwhile; endif; ?>
-	</div>
+
 
 </div>
 
