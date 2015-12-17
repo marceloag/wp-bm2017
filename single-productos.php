990fcb6ae50a3faf4ed1f7c1fd9bb341c6e0bd3d
Marcelo Aguila <web@croop.cl>
2015-12-17 10:42:24 -0300

    Single

--- single-productos.php
+++ single-productos.php
@@ -5,7 +5,7 @@
 		<div class="large-12 columns large-centered">
 			<div class="large-7 columns">
 				<!-- <?php the_post_thumbnail("producto"); ?> -->
-			<ul class="example-orbit" data-orbit>
+			<ul class="example-orbit" data-orbit data-options="slide_number:false">
 				<?php
 	              	$attachments = get_children(array('post_parent' => $post->ID,
 	                                      'post_status' => 'inherit',
