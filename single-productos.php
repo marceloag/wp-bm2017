b3aa50ec320f0f3a6ac0649506a90ec7076a96c1
Marcelo Aguila <web@croop.cl>
2015-12-17 09:38:32 -0300

    Single

--- single-productos.php
+++ single-productos.php
@@ -1,10 +1,12 @@
 <?php get_header(); ?>
 
-<div class="large-10 columns large-centered">
-	
-	<h3><?php the_title(); ?></h3>
-	<?php the_content(); ?>
+<div class="row">
+	<div class="large-10 columns large-centered">
 
+		<h3><?php the_title(); ?></h3>
+		<?php the_content(); ?>
+
+	</div>
 </div>
 
 <?php get_footer(); ?>
