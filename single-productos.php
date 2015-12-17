c0528ae060d793b748677b221c5fddc96c17567d
Marcelo Aguila <web@croop.cl>
2015-12-17 09:39:45 -0300

    Single

--- single-productos.php
+++ single-productos.php
@@ -1,8 +1,10 @@
 <?php get_header(); ?>
 
 <div class="row">
-	<div class="large-10 columns large-centered">
+	<div class="large-8 columns large-centered">
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+		<?php the_post_thumbnail(); ?>
 		<h3><?php the_title(); ?></h3>
 
 		<?php the_content(); ?>
