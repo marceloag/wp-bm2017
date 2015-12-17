34274d15258f85913f15b460d3429a4264c6f219
Marcelo Aguila <web@croop.cl>
2015-12-17 09:42:03 -0300

    Single

--- single-productos.php
+++ single-productos.php
@@ -7,8 +7,10 @@
 	<div class="large-7 columns">
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<h3><?php the_title(); ?></h3>
-
 		<?php the_content(); ?>
+		<a href="" class="button tiny alert comprar">
+			<i  class="fa fa-cart"></i> Comprar
+		</a>
 	<?php endwhile; endif; ?>
 	</div>
 
