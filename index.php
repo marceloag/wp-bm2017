ce34ea56315be633b69c3a8d9b6e89555f1ba2f7
Marcelo Aguila <web@croop.cl>
2018-04-25 09:14:38 -0300

    cambios

--- index.php
+++ index.php
@@ -2,153 +2,138 @@
 
 
 <section>
-    <!-- SLIDER --> 
+    <!-- SLIDER -->
     <div class="row collapse full">
         <div class="large-12 columns">
               <ul class="example-orbit" data-orbit data-options="bullets:false;slide_number:false">
-       
+
               <li class="active">
                 <img src="<?php bloginfo('template_directory'); ?>/img/slider_1.png" alt="slide 1" />
-             
+
                      <div class="caption">
-                       <h1 class="titulo_slider">Lorem Imput</h1>
+                       <h1 class="titulo_slider">Producción Audiovisual</h1>
                             <p class="texto_slider">
-                              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.
-                            </p>
-                     </div>
-               </li>
-               
-               <li class="active">
-                <img src="<?php bloginfo('template_directory'); ?>/img/slider_1.png" alt="slide 1" />
-             
-                     <div class="caption">
-                       <h1 class="titulo_slider">Lorem Imput</h1>
-                            <p class="texto_slider">
-                              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.
+															Creativos, innovadores, transgresores, amantes de la Buena Mesa y que sin duda cada vez que nos confíen algún trabajo, ahí estaremos para cumplir tu  encargo. Sin dejar ningún rastro por más  difícil que sea, porque sellamos un compromiso.
                             </p>
                      </div>
                </li>
             </ul>
-            
-            
-            
-            
         </div>
     </div>
-    
-     <!-- QUIENES SOMOS --> 
+
+     <!-- QUIENES SOMOS -->
     <div class="row full collapse">
         <div class="large-6 columns quienes_somos">
             <h1 class="titulo_quienes_somos">Productora <br> Buenos Muchachos</h1>
-            
+
             <p class="texto_quienes_somos">Productora Buenos Muchachos, es una empresa regional, de iniciativa privada, creada en diciembre de 2008 por un grupo de personas amantes de las comunicaciones, con ganas de presentar propuestas novedosas a la comunidad, incorporando altos estándares tecnológicos en los procesos de producción.</p>
         </div>
-        
+
         <div class="large-6 columns quienes_somos">
             <img src="<?php bloginfo('template_directory'); ?>/img/img_quienes%20somos.png" alt="" class="img_quienes_somos">
         </div>
-        
+
     </div>
-    
+
      <!-- SERVICIOS -->
      <div class="row collapse servicios">
          <div class="large-12 columns text-center">
-          
+
            <h1><spam>Nuestros</spam> Servicios</h1>
-           
+
              <ul class="large-block-grid-3  medium-block-grid-3  small-block-grid-1 iconos">
-            
-                   <li> 
+
+                   <li>
                       <img src="<?php bloginfo('template_directory'); ?>/img/icon_cinema.png" alt="" class="icon_servicios">
                     <a href="#"><h4 class="texto_servicio">VIDEO <br> COORPORATIVO</h4></a>
                    </li>
-               
-                   <li> 
+
+                   <li>
                       <img src="<?php bloginfo('template_directory'); ?>/img/icon_publicidad.png" alt="" class="icon_servicios">
                         <a href="#"><h4 class="texto_servicio">PUBLICIDAD</h4></a>
                    </li>
-               
-                   <li> 
+
+                   <li>
                       <img src="<?php bloginfo('template_directory'); ?>/img/icon_video.png" alt="" class="icon_servicios">
                      <a href="#"> <h4 class="texto_servicio">OTRAS <br> PRESTACIONES</h4></a>
                    </li>
-            
+
               </ul>
           </div>
       </div>
-      
+
      <!-- PORTAFOLIO -->
      <div class="row full textura">
          <div class="large-12 columns text-center">
              <h1>Portafolio</h1>
-             
+
              <div class="owl-carousel owl-theme">
-    
+
                 <div class="item-video" data-merge="2"><iframe width="460" height="315"     src='https://www.youtube.com/embed/iUxpou8nuhQ' frameborder='0' allowfullscreen></iframe><p class="texto_video">Spot Comercial Serrano Shell Punta Arenas</p></div>
-    
+
                 <div class="item-video" data-merge="2"><iframe width="460" height="315"     src="https://www.youtube.com/embed/wI3hQOZ9AM0" frameborder="0" allowfullscreen></iframe><p class="texto_video">Spot suzuki swift sport - Recasur dercocenter</p></div>
-    
+
                 <div class="item-video" data-merge="2"><iframe width="460"             height="315"src="https://www.youtube.com/embed/iUxpou8nuhQ" frameborder="0" allowfullscreen</iframe><p class="texto_video">Spot Comercial Serrano Shell Punta Arenas</p></div>
-     
+
                 <div class="item-video" data-merge="2"><iframe width="460" height="315"     src="https://www.youtube.com/embed/wI3hQOZ9AM0" frameborder="0" allowfullscreen></iframe><p class="texto_video">Spot suzuki swift sport - Recasur dercocenter</p></div>
-    
+
                 <div class="item-video" data-merge="2"><iframe width="460" height="315"     src="https://www.youtube.com/embed/iUxpou8nuhQ" frameborder="0" allowfullscreen></iframe><p class="texto_video">Spot Comercial Serrano Shell Punta Arenas</p></div>
-     
+
                 <div class="item-video" data-merge="2"><iframe width="460" height="315"     src="https://www.youtube.com/embed/wI3hQOZ9AM0" frameborder="0" allowfullscreen></iframe><p class="texto_video">Spot suzuki swift sport - Recasur dercocenter</p></div>
              </div>
          </div>
       </div>
-      
-     <!-- PROGRAMAS -->        
+
+     <!-- PROGRAMAS -->
      <div class="row collapse full">
          <div class="large-12 columns">
               <ul class="large-block-grid-2  medium-block-grid-2 small-block-grid-1">
-              
+
                   <li class="item">
                      <div class="row full contenedor_programa">
                          <div class="large-5 medium-5 columns logo_programa">
                              <img src="<?php bloginfo('template_directory'); ?>/img/logo_se_vende.png" alt="">
                          </div>
-                         
+
                          <div class="large-7 medium-7 columns texto_programas">
                             <h3>Se Vende</h3>
                              <p>Como vendedor o como auspiciador, nuestros clientes cuentan con una vitrina permanente, para mostrar sus productos o servicios. Este Programa <a href="#">[…]</a></p>
                          </div>
                      </div>
                   </li>
-                  
+
                   <li class="item">
                      <div class="row full contenedor_programa">
                          <div class="large-5 medium-5 columns logo_programa">
                              <img src="<?php bloginfo('template_directory'); ?>/img/logo_buena_mesa.png" alt="">
                          </div>
-                         
+
                          <div class="large-7 medium-7 columns texto_programas">
                             <h3>Buena Mesa</h3>
                              <p>Con la conducción del reconocido Chef Sebastián, se exhiben distintas preparaciones culinarias para el deleite de los paladares magallánicos. Con <a href="#">[…]</a></p>
                          </div>
                      </div>
                   </li>
-                  
+
                   <li class="item">
                      <div class="row full contenedor_programa">
                          <div class="large-5 medium-5 columns logo_programa">
                              <img src="<?php bloginfo('template_directory'); ?>/img/logo_tiempo.png" alt="">
                          </div>
-                         
+
                          <div class="large-7 medium-7 columns texto_programas">
                             <h3>El Tiempo</h3>
                              <p>Primer programa regional en exhibir, con la técnica Chroma Key, el pronóstico del tiempo en la región. Con la conducción <a href="#">[…]</a></p>
                          </div>
                      </div>
                   </li>
-                  
+
                   <li class="item">
                      <div class="row full contenedor_programa">
                          <div class="large-5 medium-5 columns logo_programa">
                              <img src="<?php bloginfo('template_directory'); ?>/img/logo_rally.png" alt="">
                          </div>
-                         
+
                          <div class="large-7 medium-7 columns texto_programas">
                             <h3>Hablemos de Rally</h3>
                              <p>Programa de televisión, con la conducción de Pedro Maldonado y Yakov Masle, dedicado a la cobertura del rally regional, espacio <a href="#">[…]</a></p>
@@ -158,14 +143,14 @@
 
               </ul>
          </div>
-      </div> 
-      
-     <!-- CLIENTES --> 
+      </div>
+
+     <!-- CLIENTES -->
      <div class="row full">
         <div class="large-12 columns text-center">
             <h1><spam>Nuestros</spam> Clientes</h1>
-        
-         <div class="large-10 columns large-centered logos_publicidad">        
+
+         <div class="large-10 columns large-centered logos_publicidad">
             <div class="owl-carousel owl-theme">
                 <div class="logos"> <img src="<?php bloginfo('template_directory'); ?>/img/logo_tv_red.png" alt=""> </div>
                 <div class="logos"> <img src="<?php bloginfo('template_directory'); ?>/img/logo_chile_crece.png" alt=""> </div>
@@ -177,8 +162,8 @@
             </div>
           </div>
         </div>
-    </div> 
-    
+    </div>
+
      <!-- CONTACTO -->
      <div class="row full collapse contacto">
          <div class="large-6 columns textura_contacto">
@@ -190,16 +175,16 @@
                   <textarea placeholder="*Mensaje..."></textarea>
                   <a href=""><button>Enviar</button></a>
                </div>
-                     
-             
+
+
           </div>
-         
+
          <div class="large-6 columns">
             <div class="embed-container">
              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2392.3347501771045!2d-70.90239268417355!3d-53.15803276337921!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbdb2637f592fc0d9%3A0x1432269e02dd9821!2sCroop+Agencia+Digital!5e0!3m2!1ses!2scl!4v1492789219234" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></div>
          </div>
-     </div>                             
-</section>   
+     </div>
+</section>
 
 
 <?php get_footer(); ?>
