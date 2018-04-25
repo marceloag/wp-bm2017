9a4a845236e70b90ad051f3739ce8ec21b240cc4
Marcelo Aguila <web@croop.cl>
2018-04-25 10:47:19 -0300

    ca ca cambios

--- page.php
+++ page.php
@@ -4,6 +4,7 @@
     <div class="row page">
       <div class="large-10 large-centered columns">
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<h1><?php the_title() ?></h1>
 					<?php the_content(); ?>
 				<?php endwhile; endif; ?>
       </div>
