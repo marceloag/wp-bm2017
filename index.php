31163cb54e5b14d89d904153d7d3bc90a48a001d
Marcelo Aguila <web@croop.cl>
2020-07-13 16:13:13 -0300

    video

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <div class="row collapse full">
 
     <style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'>
-                                                <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/P_Gv7dCpLLk?controls=0?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/P_Gv7dCpLLk?controls=0?autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <!-- <div class="large-12 columns"> -->
               <!--<ul class="example-orbit" data-orbit data-options="bullets:false;slide_number:false">
                <li class="active">
