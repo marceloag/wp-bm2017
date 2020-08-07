6d4f4bd86d9567e64e83086a4554a177c364de44
Marcelo Aguila <web@croop.cl>
2020-08-07 09:21:59 -0300

    portafolio

--- page-portafolio.php
+++ page-portafolio.php
@@ -60,6 +60,30 @@ Template Name: Portafolio
             </ul>
           </div>
 
+          <div class="registros">
+            <h1> Spots Publicitarios </h1>
+            <ul class="large-block-grid-3">
+                            <?php
+                                $the_query = new WP_Query(array(
+                                  'post_type' => 'spots',
+                                  'showposts' => 10, 
+                                  'category_name' => 'registros',
+                                  'orderby'     => 'meta_value_num',
+                                  'order'       => 'DESC'
+                                ));
+                            ?>
+                            
+                            <!-- the loop -->
+                    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                    <li>
+                      <div class="flex-video">
+                        <?php the_content();?>
+                      </div>
+                    </li>
+                    <?php endwhile; ?>
+            </ul>
+          </div>
+
 				<?php endwhile; endif; ?>
       </div>
     </div>
