ecd7ff03eda476f3c5305c80fc8cfa74f43089ac
Marcelo Aguila <web@croop.cl>
2020-08-07 09:18:40 -0300

    portafolio

--- page-portafolio.php
+++ page-portafolio.php
@@ -10,15 +10,40 @@ Template Name: Portafolio
     <div class="row page">
       <div class="large-12 large-centered columns">
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					<h1><?php the_title() ?></h1>
+					<!-- <h1><?php the_title() ?></h1> -->
 
           <div class="registros">
-            <ul class="large-block-grid-4">
+            <h1> Registros Audiovisuales </h1>
+            <ul class="large-block-grid-3">
                             <?php
                                 $the_query = new WP_Query(array(
                                   'post_type' => 'post',
                                   'showposts' => 10, 
                                   'category_name' => 'registros',
+                                  'orderby'     => 'meta_value_num',
+                                  'order'       => 'DESC'
+                                ));
+                            ?>
+                            
+                            <!-- the loop -->
+                    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                    <li>
+                      <div class="flex-video">
+                        <?php the_content();?>
+                      </div>
+                    </li>
+                    <?php endwhile; ?>
+            </ul>
+          </div>
+
+          <div class="circuito">
+            <h1> Circuito Cerrado </h1>
+            <ul class="large-block-grid-3">
+                            <?php
+                                $the_query = new WP_Query(array(
+                                  'post_type' => 'post',
+                                  'showposts' => 10, 
+                                  'category_name' => 'circuito',
                                   'orderby'     => 'meta_value_num',
                                   'order'       => 'DESC'
                                 ));
